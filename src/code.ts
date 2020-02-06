62ad1cb37ad71922454495041a4a4023ff5c4e47
Ivan Harasta <harasta.ivan@gmail.com>
2020-02-06 14:23:20 +0100

    Subject

--- src/code.ts
+++ src/code.ts
@@ -1,16 +1,28 @@
-import { Observable } from 'rxjs/Observable';
-import { Subscription, fromEvent } from 'rxjs';
-import 'rxjs/add/operator/share';
+import { Subject } from 'rxjs/Subject';
+import { Subscription } from 'rxjs';
 
-const addItem = (val: string) => {
+const addItem = (val: String) => {
     const node: Element = document.createElement('li');
-    const textnode: Text = document.createTextNode(val);
+    const textnode: Text = document.createTextNode(val.toString());
     node.appendChild(textnode);
     document.getElementById('output').appendChild(node);
 };
 
-const observable: Observable<Event> = fromEvent(document, 'mousemove');
+const subject: Subject<String> = new Subject();
 
-setTimeout(() => {
-    const subscription: Subscription = observable.subscribe((x: any) => addItem(x));
-}, 2000);
+subject.subscribe(
+    (data: any) => addItem('Observer 1: ' + data),
+    (err) => addItem(err),
+    () => addItem('Observer 1 Completed')
+);
+
+subject.next('The first thing has been sent');
+
+const subscription2: Subscription = subject.subscribe((data: String) => addItem('Observer 2: ' + data));
+
+subject.next('The second thing has been sent');
+subject.next('The third thing has been sent');
+
+subscription2.unsubscribe();
+
+subject.next('The final thing has been sent');
