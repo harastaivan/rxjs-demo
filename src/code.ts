25662d28b91e701da1f371e363ec139eec25fcb5
Ivan Harasta <harasta.ivan@gmail.com>
2020-02-06 14:13:50 +0100

    Event observable

--- src/code.ts
+++ src/code.ts
@@ -1,33 +1,16 @@
 import { Observable } from 'rxjs/Observable';
-import { Observer, Subscription } from 'rxjs';
+import { Subscription, fromEvent } from 'rxjs';
 import 'rxjs/add/operator/share';
 
 const addItem = (val: string) => {
-    const node = document.createElement('li');
-    const textnode = document.createTextNode(val);
+    const node: Element = document.createElement('li');
+    const textnode: Text = document.createTextNode(val);
     node.appendChild(textnode);
     document.getElementById('output').appendChild(node);
 };
 
-const observable: Observable<string> = Observable.create((observer: Observer<string>) => {
-    try {
-        observer.next('Hey guys');
-        // throw new Error('Something went wrong');
-        observer.next('How are you');
-        setInterval(() => {
-            observer.next('I am good');
-        }, 2000);
-    } catch (err) {
-        observer.error(err);
-    }
-}).share(); // warm/hot observable
-
-const subscription: Subscription = observable.subscribe(
-    (x: string) => addItem(x),
-    (error: Error) => addItem(`${error.name}: ${error.message}`),
-    () => addItem('Completed')
-);
+const observable: Observable<Event> = fromEvent(document, 'mousemove');
 
 setTimeout(() => {
-    const subscription2 = observable.subscribe((x: string) => addItem('Subscriber 2: ' + x));
-}, 1000);
+    const subscription: Subscription = observable.subscribe((x: any) => addItem(x));
+}, 2000);
