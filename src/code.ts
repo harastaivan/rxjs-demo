6dae2dfe826811d0ccf9e01a0736a54ba139e774
Ivan Harasta <harasta.ivan@gmail.com>
2020-02-06 14:26:57 +0100

    BehaviorSubject

--- src/code.ts
+++ src/code.ts
@@ -1,4 +1,4 @@
-import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Subscription } from 'rxjs';
 
 const addItem = (val: String) => {
@@ -8,7 +8,7 @@ const addItem = (val: String) => {
     document.getElementById('output').appendChild(node);
 };
 
-const subject: Subject<String> = new Subject();
+const subject: BehaviorSubject<String> = new BehaviorSubject('First');
 
 subject.subscribe(
     (data: any) => addItem('Observer 1: ' + data),
@@ -17,6 +17,7 @@ subject.subscribe(
 );
 
 subject.next('The first thing has been sent');
+subject.next('...Observer 2 is about to subscribe');
 
 const subscription2: Subscription = subject.subscribe((data: String) => addItem('Observer 2: ' + data));
 
